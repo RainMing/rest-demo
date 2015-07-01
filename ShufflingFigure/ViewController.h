75f517796e0ce91cd8a683e15400e64c9bf2fd92
Lan <qs69m2s@163.com>
2015-07-01 17:48:06 +0800

    Initial Commit

---
+++ ShufflingFigure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShufflingFigure
+//
+//  Created by lanouhn on 15/7/1.
+//  Copyright (c) 2015年 main.m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShufflingFigure/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShufflingFigure
+//
+//  Created by lanouhn on 15/7/1.
+//  Copyright (c) 2015年 main.m. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
